a81cac692d8720f718d735de3ef87b1513a80595
211125 <211125@ids.upchiapas.edu.mx>
2023-02-28 20:53:18 -0600

    Initial commit

---
+++ src/main/java/com/demo/demo/dto/request/UpdateStudentRequest.java
@@ -0,0 +1,41 @@
+package com.demo.demo.dto.request;
+
+public class UpdateStudentRequest {
+
+    private String name;
+    private String apellido;
+    private String name2;
+    private String apellido2;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getName2() {
+        return name2;
+    }
+
+    public void setName2(String name2) {
+        this.name2 = name2;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+}
